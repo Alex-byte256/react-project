af09a0e850258987a356d1f014b13083884a1a23
Alex-byte256 <56194044+Alex-byte256@users.noreply.github.com>
2022-02-01 00:12:19 +0300

    learn css module

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import s from'./Header.module.css';
+
+const Header = () =>{
+	return(
+		<header className={s.header}>
+		<img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_3.png"></img>
+	 </header>
+	);
+}
+
+export default Header;
