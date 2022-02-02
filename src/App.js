8a5f78292753a60a27ea96e022b89fa99d3f1c24
Alex-byte256 <56194044+Alex-byte256@users.noreply.github.com>
2022-02-02 03:58:33 +0300

    Route components for Navbar

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route element={<Profile />} path="profile" />
+            <Route element={<Dialogs />} path="dialogs" />
+            <Route element={<News />} path="news" />
+            <Route element={<Music />} path="music" />
+            <Route element={<Settings />} path="settings" />
+          </Routes>
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 };
 
