aad088e29622b92a305c6f8b6c7e03b54b07ab53
Alex-byte256 <56194044+Alex-byte256@users.noreply.github.com>
2022-02-03 00:33:25 +0300

    change dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) =>{
 	return (
-<div className={s.content}>
-Dialogs
+<div className={s.dialogs}>
+<div className={s.dialogs_items} >
+	<div className={s.dialog + ' ' + s.active} >
+		Alex
+	</div>
+	<div className={s.dialog} >
+		Bohdan
+	</div>
+	<div className={s.dialog} >
+		Maxim
+	</div>
+	<div className={s.dialog} >
+		Vanya
+	</div>
+	<div className={s.dialog} >
+		Victor
+	</div>
+	<div className={s.dialog} >
+		Yulian
+	</div>
+</div>
+<div className={s.messages}>
+	<div className={s.message}>Hi</div>
+	<div className={s.message}>How is your react</div>
+	<div className={s.message}>yo</div>
+</div>
 </div>
 );
 }
