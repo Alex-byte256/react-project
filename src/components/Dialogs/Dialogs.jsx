7af50b656a99956b12ffb826f54167357a812d22
Alex-byte256 <56194044+Alex-byte256@users.noreply.github.com>
2022-02-03 03:14:34 +0300

    object in array refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,22 +20,33 @@ const Message = (props) =>{
 
 
 const Dialogs = (props) =>{
+
+	let dialogsData = [
+		{id:1, name:"Alex"},
+		{id:2, name:"Bohdan"},
+		{id:3, name:"Maxim"},
+		{id:4, name:"Vanya"},
+		{id:5, name:"Victor"},
+		{id:6, name:"Yulian"}
+	]
+
+	let messagesData = [
+		{id:1, message:"Hi"},
+		{id:2, message:"How is your react"},
+		{id:3, message:"yo"},
+		{id:4, message:"yo"},
+		{id:5, message:"yo"}
+	]
+
 	return (
 <div className={s.dialogs}>
 <div className={s.dialogs_items} >
-	<DialogItem name="Alex" id="1" />	
-	<DialogItem name="Bohdan" id="2" />	
-	<DialogItem name="Maxim" id="3" />	
-	<DialogItem name="Vanya" id="4" />	
-	<DialogItem name="Victor" id="5" />	
-	<DialogItem name="Yulian" id="6" />	
+	<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />	
+	<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />	
 </div>
 <div className={s.messages}>
-	<Message message="Hi" />
-	<Message message="How is your react" />
-	<Message message="yo" />
-	<Message message="yo" />
-	<Message message="yo" />
+	<Message message={messagesData[0].message} />
+	<Message message={messagesData[1].message} />
 </div>
 </div>
 );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () =>{
+
+	let postData = [
+		{id:1, message:"Hi, how are you?", likesCount: 12},
+		{id:2, message:"I'ts my first post", likesCount: 11}
+	]
+
 	return(
         <div className={s.postsBlock} >
           <h3>My post</h3>
@@ -14,8 +20,8 @@ const MyPosts = () =>{
 				 </div>
 			 </div>
 			 <div className={s.posts}>
-          	<Post message='Hi, how are you?' like='13' />
-          	<Post message="I'ts my first post" like='43' />
+          	<Post message={postData[0].message} like={postData[0].likesCount} />
+				 <Post message={postData[1].message} like={postData[1].likesCount} />
 			 </div>
         </div>
 	);
