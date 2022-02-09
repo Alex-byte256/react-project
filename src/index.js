671b2f1c175ec5465126bff81295080f1b115f5e
Alex-byte256 <56194044+Alex-byte256@users.noreply.github.com>
2022-02-10 01:05:22 +0300

    make store and use this for _state

--- src/index.js
+++ src/index.js
@@ -3,38 +3,32 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import state, { subscribe } from "./redux/state";
 import "./index.css";
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateNewMessageText,
-} from "./redux/state";
+import store from "./redux/state";
 
 let rerenderEntireTree = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App
-        addPost={addPost}
-        posts={state.profilePage.postData}
-        dialogs={state.dialogsPage.dialogsData}
-        messages={state.dialogsPage.messagesData}
-        friends={state.friendPage.avatarData}
-        postText={state.profilePage.newPostText}
-        updateNewPostText={updateNewPostText}
-        addMessage={addMessage}
-        messageText={state.dialogsPage.newMessageText}
-        updateNewMessageText={updateNewMessageText}
+        addPost={store.addPost.bind(store)}
+        posts={store.getState().profilePage.postData}
+        dialogs={store.getState().dialogsPage.dialogsData}
+        messages={store.getState().dialogsPage.messagesData}
+        friends={store.getState().friendPage.avatarData}
+        postText={store.getState().profilePage.newPostText}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        messageText={store.getState().dialogsPage.newMessageText}
+        updateNewMessageText={store.updateNewMessageText.bind(store)}
       />
     </React.StrictMode>,
     document.getElementById("root")
   );
 };
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,101 +1,108 @@
-let rerenderEntireTree = () => {
-  console.log("state was changed");
-};
-
-let state = {
-  profilePage: {
-    postData: [
-      { id: 1, message: "Hi, how are you?", likesCount: 12 },
-      { id: 2, message: "I'ts my first post", likesCount: 11 },
-      { id: 3, message: "I'm use arr map", likesCount: 18 },
-      { id: 4, message: "I'ts work", likesCount: 23 },
-    ],
-    newPostText: "this area take value from state",
+let store = {
+  _state: {
+    profilePage: {
+      postData: [
+        { id: 1, message: "Hi, how are you?", likesCount: 12 },
+        { id: 2, message: "I'ts my first post", likesCount: 11 },
+        { id: 3, message: "I'm use arr map", likesCount: 18 },
+        { id: 4, message: "I'ts work", likesCount: 23 },
+      ],
+      newPostText: "this area take value from state",
+    },
+    dialogsPage: {
+      messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How is your react" },
+        { id: 3, message: "yo" },
+        { id: 4, message: "yo" },
+        { id: 5, message: "yo" },
+      ],
+      newMessageText: "message from state",
+      dialogsData: [
+        { id: 1, name: "Alex" },
+        { id: 2, name: "Bohdan" },
+        { id: 3, name: "Maxim" },
+        { id: 4, name: "Vanya" },
+        { id: 5, name: "Victor" },
+        { id: 6, name: "Yulian" },
+      ],
+    },
+    friendPage: {
+      avatarData: [
+        {
+          id: 1,
+          name: "Alex",
+          img: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
+        },
+        {
+          id: 2,
+          name: "Bohdan",
+          img: "https://img2.freepng.ru/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg",
+        },
+        {
+          id: 3,
+          name: "Stepan",
+          img: "https://cdn-icons-png.flaticon.com/512/236/236832.png",
+        },
+        {
+          id: 4,
+          name: "Robert",
+          img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkcMKh11l9FLRwa2kTls-6aFrVZPyiZZ6RKBuCUpHruEvKcw9-Y37ckv-ZrlxCLTS4Vw0&usqp=CAU",
+        },
+        {
+          id: 5,
+          name: "Ivan",
+          img: "https://cdn-icons-png.flaticon.com/512/194/194931.png",
+        },
+      ],
+    },
   },
-  dialogsPage: {
-    messagesData: [
-      { id: 1, message: "Hi" },
-      { id: 2, message: "How is your react" },
-      { id: 3, message: "yo" },
-      { id: 4, message: "yo" },
-      { id: 5, message: "yo" },
-    ],
-    newMessageText: "message from state",
-    dialogsData: [
-      { id: 1, name: "Alex" },
-      { id: 2, name: "Bohdan" },
-      { id: 3, name: "Maxim" },
-      { id: 4, name: "Vanya" },
-      { id: 5, name: "Victor" },
-      { id: 6, name: "Yulian" },
-    ],
+  getState() {
+    return this._state;
   },
-  friendPage: {
-    avatarData: [
-      {
-        id: 1,
-        name: "Alex",
-        img: "https://cdn-icons-png.flaticon.com/512/147/147144.png",
-      },
-      {
-        id: 2,
-        name: "Bohdan",
-        img: "https://img2.freepng.ru/20180523/tha/kisspng-businessperson-computer-icons-avatar-clip-art-lattice-5b0508dc6a3a10.0013931115270566044351.jpg",
-      },
-      {
-        id: 3,
-        name: "Stepan",
-        img: "https://cdn-icons-png.flaticon.com/512/236/236832.png",
-      },
-      {
-        id: 4,
-        name: "Robert",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkcMKh11l9FLRwa2kTls-6aFrVZPyiZZ6RKBuCUpHruEvKcw9-Y37ckv-ZrlxCLTS4Vw0&usqp=CAU",
-      },
-      {
-        id: 5,
-        name: "Ivan",
-        img: "https://cdn-icons-png.flaticon.com/512/194/194931.png",
-      },
-    ],
+  _rerenderEntireTree() {
+    console.log("state was changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.postData.push(newPost);
+    this._state.profilePage.newPostText = "";
+    this._rerenderEntireTree(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._rerenderEntireTree(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 6,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._rerenderEntireTree(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 6,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._state.dialogsPage.newMessageText = "";
+    this._rerenderEntireTree(this._state);
+  },
+  updateNewMessageText(newMessage) {
+    this._state.dialogsPage.newMessageText = newMessage;
+    this._rerenderEntireTree(this._state);
+  },
+  subscribe(observer) {
+    this._rerenderEntireTree = observer;
   },
 };
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.postData.push(newPost);
-  state.profilePage.newPostText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-export const addMessage = () => {
-  let newMessage = {
-    id: 6,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  state.dialogsPage.newMessageText = "";
-  rerenderEntireTree(state);
-};
-
-export const updateNewMessageText = (newMessage) => {
-  state.dialogsPage.newMessageText = newMessage;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-
-window.state = state;
-
-export default state;
+export default store;
+window.store = store;
